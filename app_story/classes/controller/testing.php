ec2d07d44bd77991f7e2671f59c2015d12db8013
Patrick Forringer <destos@gmail.com>
2010-12-16 14:39:35 -0600

    twilio testing controller.

--- app_story/classes/controller/testing.php
+++ app_story/classes/controller/testing.php
@@ -7,6 +7,7 @@ class Controller_Testing extends Controller_Twilio {
 	
 		echo '<pre>';
 		$twilio = Twilio::instance();
+		$test = array();
 		//$test['get_sandbox'] = $twilio->get_sandbox();
 		//$test['get_account'] = $twilio->get_account();
 		//$test['set_account'] = $twilio->set_account_name('Patrick\'s Twilio Account');
@@ -14,6 +15,21 @@ class Controller_Testing extends Controller_Twilio {
 		print_r( $test );
 		
 		echo '</pre>';
+		$this->auto_respond = false;
+	}
+	
+	public function action_db_check(){
+	
+		$stories = ORM::factory('story')->find_all();
+		
+		if( $stories ){
+			echo 'loaded stories ';
+			foreach( $stories as $story ){
+				//print_r($story);
+				echo $story->id.'-';
+			}
+		}
+		
 		$this->auto_respond = false;
 	}
 	
